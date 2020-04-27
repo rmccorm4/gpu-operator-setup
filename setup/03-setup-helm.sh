5a74b293a11fbc19ad2f1164ef2592e3963af019
Ryan McCormick <rmccormick@nvidia.com>
2020-04-27 23:49:17 +0000

    Update helm/gpu operator installer scripts for Helm v3
    Signed-off-by: Ryan McCormick <rmccormick@nvidia.com>

--- setup/03-setup-helm.sh
+++ setup/03-setup-helm.sh
@@ -1,24 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash
 
-# Reference: https://github.com/NVIDIA/gpu-operator/blob/0cc830a4c600411650c66b528fbd2d56da2f63a4/README.md
+# Download official Helm installer script
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 
-HELM_VERSION="v2.15.2"
+# Install Helm
+chmod 700 get_helm.sh
+./get_helm.sh
 
-# Download and install Helm
-TMPDIR="$(mktemp -d)"
-curl -L https://git.io/get_helm.sh -o "${TMPDIR}/get_helm.sh"
-bash "${TMPDIR}/get_helm.sh" --version "${HELM_VERSION}"
-
-# Create service-account for helm
-kubectl create serviceaccount -n kube-system tiller
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-
-# Initialize Helm
-helm init --service-account tiller --upgrade --wait
-
-# Note that if you have helm already deployed in your cluster and you are adding a new node, run this instead
-#helm init --client-only
-
-# Additional step required for Kubernetes v1.16. See: https://github.com/helm/helm/issues/6374
-helm init --service-account tiller --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
-kubectl wait --for=condition=available -n kube-system deployment tiller-deploy
+# Cleanup
+rm get_helm.sh
--- setup/04-setup-gpu-operator.sh
+++ setup/04-setup-gpu-operator.sh
@@ -7,20 +7,15 @@ sudo modprobe -a i2c_core ipmi_msghandler
 # Have modules load on reboot
 echo -e "i2c_core\nipmi_msghandler" | sudo tee /etc/modules-load.d/driver.conf
 
-# Before running this, make sure helm is installed and initialized:
+# Add the NVIDIA repo:
 helm repo add nvidia https://nvidia.github.io/gpu-operator
 helm repo update
 
 # Note that after running this command, NFD will be automatically deployed. If you have NFD already setup, follow the NFD instruction from the Prerequisites.
-helm install --devel nvidia/gpu-operator -n test-operator --wait
-kubectl apply -f https://raw.githubusercontent.com/NVIDIA/gpu-operator/master/manifests/cr/sro_cr_sched_none.yaml
-
-SLEEP_TIME=60
-echo "Sleeping for ${SLEEP_TIME} seconds to let pods start up..."
-sleep ${SLEEP_TIME}
-
-# Check GPU operator resources to see if pods are running
-kubectl get pods --namespace gpu-operator-resources
+helm install --devel nvidia/gpu-operator --wait --generate-name
 
 # To check the gpu-operator version
 helm ls
+
+# Check the status of the pods to ensure all the containers are running. A sample output is shown below in the cluster
+kubectl get pods -A
